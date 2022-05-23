7fb65399237972a0e855f613e019a8b164a4e211
Assil <assilmahfoudi@gmail.com>
2022-05-23 12:42:28 +0200

    Robot.c made

--- CODE/uC_ROBOT/Objects/robot.d
+++ CODE/uC_ROBOT/Objects/robot.d
@@ -1,7 +1,7 @@
-./objects/system_lpc17xx.o: RTE\Device\LPC1769\system_LPC17xx.c \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+./objects/robot.o: ROBOT.C \
   C:\Users\assil\AppData\Local\Arm\Packs\Keil\LPC1700_DFP\2.7.1\Device\Include\LPC17xx.h \
   C:\Users\assil\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
   C:\Users\assil\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
   C:\Users\assil\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
   C:\Users\assil\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
---
+++ CODE/uC_ROBOT/ROBOT.C
@@ -0,0 +1,135 @@
+#include "LPC17xx.h"
+
+int T0clk =25000000;
+int i =0 ;
+int T_mes=10;
+int mes=0 ;
+
+int f ;
+
+
+void init_Telemetre ()
+	{
+// Initialisation of the pins 
+		
+//Initialisation of the output 		
+		LPC_PINCON->PINSEL0&=~(3);
+		LPC_GPIO0-> FIODIR|=1;
+		LPC_GPIO0-> FIOPIN|=1;
+		
+//Initialisation of the switches 	(Meas frequency )	
+		
+		LPC_PINCON->PINSEL0&=~((3<<2)|(3<<4));
+		LPC_GPIO0-> FIODIR&=~(3<<1);
+		mes=(LPC_GPIO0-> FIOPIN)&(3<<1);
+		mes=mes>>1;
+		mes=mes^3;
+		
+switch(mes)
+{
+  case 0:
+  T_mes=10;
+  break;
+  case 1:
+  T_mes=15;
+  break;
+  case 2:
+  T_mes=20;
+  break;
+  case 3:
+  T_mes=25;
+  break;
+	
+  default:
+	T_mes=10;
+}
+		
+
+		
+		
+// Initialisation of the timer 0 for Ultrasound emition !
+
+	// Falling edge to start emission 	
+		
+		LPC_TIM0->MR0=0;
+		LPC_TIM0->MCR|=1;
+		LPC_TIM0->MCR&=~(3<<1);
+		
+	// Rising edge to end the pulse 	
+	
+		LPC_TIM0->MR1=200;
+		LPC_TIM0->MCR|=(1<<3);
+		LPC_TIM0->MCR&=~(3<<4);
+
+	// Reset	
+		LPC_TIM0->MR2=((1/((float)T_mes)))*T0clk;
+		LPC_TIM0->MCR|=(1<<7);
+		
+	
+		
+		NVIC_EnableIRQ(TIMER0_IRQn);
+		LPC_TIM0->TCR|=1;
+		
+		
+		//RECEPTION 
+		LPC_PINCON->PINSEL3|=(3<<20);
+		LPC_TIM0->CCR=1;
+		
+	}
+
+void TIMER0_IRQHandler()
+	{
+		LPC_TIM0->IR|=(1<<i);
+		i^=1;
+		LPC_GPIO0-> FIOPIN^=1;
+	}
+
+	void Obstacle_Warning()
+	{
+		LPC_TIM1->MR0=0.25*T0clk;
+		LPC_TIM1->MCR|=(1<<0);
+		LPC_TIM1->MCR&=~(3<<1);
+		
+		LPC_TIM1->MR1=1000*T0clk;
+		LPC_TIM1->MCR|=(3<<3);
+		LPC_TIM1->MCR&=~(1<<5);
+		
+		NVIC_EnableIRQ(TIMER1_IRQn);
+				
+	
+	};
+	void TIMER1_IRQHandler()
+	{
+		LPC_TIM1->IR|=(1<<i);
+		i^=1;
+		
+		
+	}
+
+int main(void)
+
+float dist ;
+
+	{
+		init_Telemetre();
+		while(1)
+		{
+		dist = (349*(LPC_TIM0->CR0)/T0clk)/2;
+			
+			if (dist>100)
+			{
+				// don't emmit the sound
+				LPC_TIM1->TCR=2;
+			}
+			else
+				if (dist>20)
+			{
+				LPC_TIM1->TCR=1;
+				f=(10-(d/0.1))*0.25+0.5;
+			}
+			  LPC_TIM1->MR1=(1/(float)f)*T0clk;
+			
+		}; 		// Waiting mode 
+		
+		return 0;
+	}
